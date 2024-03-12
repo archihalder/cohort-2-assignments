5ce5c0d897973bb1f10a78f0e82034608f70f6a8
Archi Halder <archihalder23026558@gmail.com>
2024-03-12 12:24:01 +0530

    minor changes

--- week-01/assignments/medium/times.js
+++ week-01/assignments/medium/times.js
@@ -9,7 +9,7 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    const startTime = performance.now()
+    const startTime = performance.now();
     let sum = 0;
     for (let i = 1; i <= n; i++) {
         sum += i;
