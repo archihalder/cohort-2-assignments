eac16c66626d38dbf61288a8a454f6275cb9d954
Archi Halder <archihalder23026558@gmail.com>
2024-03-12 14:21:07 +0530

    added two types of counter

--- week-02/01-async-js/easy/counter1.js
+++ week-02/01-async-js/easy/counter1.js
@@ -1,23 +1,25 @@
 // We have already covered this in the second lesson, but as an easy recap try to code a counter in Javascript
 // It should go up as time goes by in intervals of 1 second
 
-// console.log("Counter to 10");
-
-function counter() {
+// counts till given end value
+function counterTo(end) {
     let start = 1;
     const intervalId = setInterval(function () {
         console.log(start);
-        if (start === 5) {
+        if (start === end) {
             clearInterval(intervalId);
         }
         start++;
     }, 1000);
 }
 
-counter();
+// counter doesn't stop
+function unlimitedCounter() {
+    let i = 1;
+    setInterval(function () {
+        console.log(i);
+        i++;
+    }, 1000);
+}
 
-// let i = 1;
-// setInterval(function () {
-//     console.log(i);
-//     i++;
-// }, 1000);
+counterTo(15);
